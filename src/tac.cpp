bedc63093f27fd89197a4ca7bfe5497fc822c8c4
Simon de Hartog <simon@fs2a.pro>
2020-06-25 20:36:32 +0200

    First operational version

--- src/tac.cpp
+++ src/tac.cpp
@@ -32,26 +32,95 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <memory>
+#include <sstream>
 #include <stdexcept>
+#include <string>
+#include <vector>
+#include <jsoncpp/json/json.h>
+#include <restclient-cpp/connection.h>
 #include <restclient-cpp/restclient.h>
+
+// This is the API key used by this application
+#define APIKEY "4252be3f9940f267ff366332859bcb2f"
 
 using std::cout, std::endl;
 
 int help()
 {
-	cout << "Usage: tac <API-key> <Trello Token>" << endl << endl;
+	cout << "Usage: tac <Trello Token>" << endl << endl;
 	cout << "For now, this application removes all archived Trello cards" << endl;
 	return 1;
 }
 
+void checkrsp(const RestClient::Response & rsp_i)
+{
+	if (rsp_i.code >= 300) {
+		throw std::runtime_error("Request failed");
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	if (argc != 3) return help();
-	(void)(argv); // Unused for now
+	if (argc != 2) return help();
 
 	RestClient::init();
 	atexit(RestClient::disable);
 
-	cout << "Not implemented yet" << endl;
+	// Ensures proper deletion upon exit
+	std::unique_ptr<RestClient::Connection> rcc;
+	RestClient::Response rsp;
+
+	rcc.reset(new RestClient::Connection("https://api.trello.com/1/"));
+
+	std::string uri;
+
+	uri = "members/me/boards?key=" APIKEY "&token=";
+	uri += argv[1];
+
+	rsp = rcc->get(uri);
+	checkrsp(rsp);
+
+	std::stringstream sss;
+	sss.str(rsp.body);
+	Json::Value data;
+	sss >> data;
+
+	for (uint16_t i = 0; i < data.size(); i++) {
+		cout << "Found " << (data[i]["closed"].asBool()?"closed":"open") << " board ";
+		cout << data[i]["name"] << " with ID " << data[i]["id"] << endl;
+
+		uri = "boards/";
+		uri += data[i]["id"].asString();
+		uri += "?cards=closed&card_fields=id,name,closed&key=" APIKEY "&token=";
+		uri += argv[1];
+		rsp = rcc->get(uri);
+		checkrsp(rsp);
+
+		sss.str(rsp.body);
+		Json::Value brdinfo;
+		sss >> brdinfo;
+		Json::Value cards = brdinfo["cards"];
+		for (uint16_t j = 0; j < cards.size(); j++) {
+			cout << "Found " << (cards[j]["closed"].asBool()?"closed":"open");
+			cout << " card \"" << cards[j]["name"].asString() << "\"";
+			cout << " with ID " << cards[j]["id"];
+
+			// Extra check, although we already filtered on closed cards
+			if (cards[j]["closed"].asBool()) {
+				cout << ", deleting it" << endl;
+				uri = "cards/";
+				uri += cards[j]["id"].asString();
+				uri += "?key=" APIKEY "&token=";
+				uri += argv[1];
+				rsp = rcc->del(uri);
+				checkrsp(rsp);
+			} else {
+				cout << ", still open so leaving it untouched" << endl;
+			}
+		}
+
+	}
+
 	return 0;
 }
